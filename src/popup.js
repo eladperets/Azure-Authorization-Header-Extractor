d49ab656948d93e69704a2cbf1b3fb19a2ebc50f
Elad Perets <elpere@microsoft.com>
2021-03-15 12:10:29 -0700

    Add option to omit bearer prefix

--- src/popup.js
+++ src/popup.js
@@ -1,20 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var tokenElem = document.getElementById('token');
-  var dateElem = document.getElementById('date_aquired');
 
-  chrome.storage.local.get('tokenObj', function(res) {
-    console.log(JSON.stringify(res));
-    tokenElem.value = res.tokenObj.token;
-    dateElem.innerHTML = 'Acquired: ' + res.tokenObj.date;
-    copyToken();
+  // Store any changes in settings
+  document.getElementById('omitBearer').addEventListener('change', function() {
+    chrome.storage.local.set({'omitBearer': this.checked})
   });
 
-  var copyElem = document.getElementById('copy');
-  copyElem.addEventListener('click', copyToken);
+  chrome.storage.local.get('omitBearer', function(shouldOmitBearer) {
+    document.getElementById('omitBearer').checked = shouldOmitBearer.omitBearer;
 
-  function copyToken() {
-    tokenElem.focus();
-    document.execCommand('SelectAll');
-    document.execCommand("Copy", false, null);
-  }
+    chrome.storage.local.get('tokenObj', function(res) {
+      var tokenElem = document.getElementById('token');
+      var dateElem = document.getElementById('date_acquired');  
+      console.log(JSON.stringify(res));
+      tokenElem.value = res.tokenObj.token;
+      if (shouldOmitBearer.omitBearer) {
+        tokenElem.value = tokenElem.value.replace('Bearer ', '');
+      }
+
+      dateElem.innerHTML = 'Acquired: ' + res.tokenObj.date;
+      document.getElementById('token').focus();
+      document.execCommand('SelectAll');
+      document.execCommand("Copy", false, null);
+    });
+  });
 });

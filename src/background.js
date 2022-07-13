2410344915582dbbb5b763253ba7e75d5a65af02
Elad Perets <elpere@microsoft.com>
2022-07-12 22:21:12 -0700

    Update manifest version + add permissions for new host
    Had to update the manifest to avoid getting the extensions being removed by Google.
    Portal is now sometimes calling ARM using edge.management.azure.com. Added permissions to the new URL.

--- src/background.js
+++ src/background.js
@@ -1,14 +1,13 @@
-
 chrome.webRequest.onSendHeaders.addListener(
   function (info) {
     if (info.requestHeaders) {
-      for(var i = 0; i < info.requestHeaders.length; i++) {
-        if(info.requestHeaders[i].name == 'Authorization' || info.requestHeaders[i].name == 'authorization') {
-          console.log(info.requestHeaders[i].value);
+      for (var i = 0; i < info.requestHeaders.length; i++) {
+        if (info.requestHeaders[i].name == 'Authorization' || info.requestHeaders[i].name == 'authorization') {
+          // console.log(info.requestHeaders[i].value);
           chrome.storage.local.set({ 'tokenObj': { 'token': info.requestHeaders[i].value, 'date': new Date().toLocaleTimeString() } });
           break;
         }
-      }      
+      }
     }
   },
   // filters
